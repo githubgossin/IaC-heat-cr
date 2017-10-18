2a849914a348b75915c684c6540f0fd5e1982877
Erik Hjelmås <erik.hjelmas@ntnu.no>
2017-10-18 08:06:59 +0200

    fixed puppet agent setup for manager

--- lib/manager_boot.bash
+++ lib/manager_boot.bash
@@ -25,10 +25,14 @@ class { 'r10k':
 EOF
 /opt/puppetlabs/bin/puppet apply /var/tmp/r10k.pp
 r10k deploy environment -p
+# temp fix to have correct fqdn for puppet, before permanent fix in dhclient
+# (which requires reboot)
 echo "$(ip a | grep -Eo 'inet ([0-9]*\.){3}[0-9]*' | tr -d 'inet ' | grep -v '^127') $(hostname).borg.trek $(hostname)" >> /etc/hosts
-/opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
 /opt/puppetlabs/bin/puppet resource service puppetserver ensure=running enable=true
-/opt/puppetlabs/bin/puppet agent -t
+/opt/puppetlabs/bin/puppet agent -t # request certificate
+/opt/puppetlabs/bin/puppet agent -t # configure manager
+/opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
+# permanent fix for domainname
 cat <<EOF >> /etc/dhcp/dhclient.conf
 supersede domain-name "borg.trek";
 supersede domain-name-servers 127.0.0.1;
